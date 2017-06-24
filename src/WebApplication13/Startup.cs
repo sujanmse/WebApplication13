3adc06570287a041509029459820db4e5fa0dd43
sujancbt <sujan.shakya@cbtassociates.com>
2017-06-24 00:20:31 -0400

    'test

--- src/WebApplication13/Startup.cs
+++ src/WebApplication13/Startup.cs
@@ -27,6 +27,7 @@ namespace WebApplication13
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            int i = 5;
             // Add framework services.
             services.AddMvc();
         }
